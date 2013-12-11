dafdafdf9b54e0e82bebbaa014b8048f26a78129
Abigail Watson <awatson1978@gmail.com>
2013-12-11 00:17:03 -0500

    Update package.js

--- package.js
+++ package.js
@@ -3,6 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+    api.use('standard-app-packages','client');
     api.use('templating','client');
 
     api.add_files('overlay.html', "client");
