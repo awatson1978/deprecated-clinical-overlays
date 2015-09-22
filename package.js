131d44b9d48d623bf72644d3f20621ae9ff78d4b
Abigail Watson <awatson1978@gmail.com>
2015-09-21 22:52:14 -0700

    Overlay object can be instantiated multiple times!

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "Add image and content overlays to a Clinical Meteor app.",
 
   // update this value before you run 'meteor publish'
-  version: "1.0.1",
+  version: "1.1.0",
 
   // if this value isn't set, meteor will default to the directory name
   name: "clinical:overlays",
@@ -13,10 +13,13 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.use('standard-app-packages@1.0.4','client');
-    api.use('templating@1.0.11','client');
+  api.use('meteor-platform');
 
-    api.add_files('overlay.html', "client");
-    api.add_files('overlay.css', "client");
-    api.add_files('overlay.js', "client");
+  api.addFiles('lib/overlay.js', "client");
+
+  api.addFiles('components/overlay/overlay.html', "client");
+  api.addFiles('components/overlay/overlay.css', "client");
+  api.addFiles('components/overlay/overlay.js', "client");
+
+  api.export('Overlay');
 });
